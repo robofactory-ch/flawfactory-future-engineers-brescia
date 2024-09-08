a7726e736c6d0c27d71db08f36cf85cb2c48002d
Damian Hardegger <92053218+spenny-the-kenny@users.noreply.github.com>
2024-09-08 17:08:24 +0200

    Refactor Arduino code for improved motor control and message parsing

--- arduino/OutputProxy/src/main.cpp
+++ arduino/OutputProxy/src/main.cpp
@@ -5,15 +5,14 @@ const int servoPin = 2;
 
 Servo servo;
 
-const int enaPin = 3;
-const int in1Pin = 7;
-const int in2Pin = 8;
+const int enaPin = 11;
+const int in1Pin = 5;
+const int in2Pin = 6;
 
 int current_speed = 0;
 int set_speed = 0;
-int acc_time = 3;
-int last_acc_time = 0;
-
+unsigned long acc_time = 20;
+unsigned long last_acc_time = 0;
 
 int middle = 70; // +55 -55
 int degree_max = 125;
@@ -21,17 +20,23 @@ int degree_min = 15;
 int current_degree = 0;
 int set_degree = 0;
 
+#define BUFFER_SIZE 64
+
+char ringBuffer[BUFFER_SIZE];
+int head = 0;
+int tail = 0;
+
 void drive(int speed)
 {
-  if (speed > 255)
+  if (speed > 200)
   {
-    speed = 255;
+    speed = 200;
   }
-  else if (speed < -255)
+  else if (speed < -200)
   {
-    speed = -255;
+    speed = -200;
   }
-  if (millis() - last_acc_time < acc_time)
+  if (millis() < last_acc_time + acc_time)
   {
     return;
   }
@@ -76,6 +81,67 @@ void steer(int angle)
   servo.write(angle);
 }
 
+void parseMessage(char *msg)
+{
+  char cmd[3]; // To store the 2-char command
+  int value = 0;
+
+  sscanf(msg, "%1s", cmd);
+
+  // skip whitespace
+  char *beg = ++msg;
+
+  while (*beg == ' ')
+  {
+    beg++;
+  }
+
+  char *end = beg;
+
+  while (*end != '\0')
+  {
+    end++;
+  }
+
+  Serial.println(cmd);
+  value = atoi(beg);
+  Serial.println(value);
+
+  if (cmd[0] == 'd')
+  {
+    set_speed = value;
+  }
+  else if (cmd[0] == 's')
+  {
+    set_degree = value;
+  }
+}
+
+void processMessage()
+{
+  // Message extraction from ring buffer
+  char message[BUFFER_SIZE];
+  int index = 0;
+  while (tail != head)
+  {
+    char currentChar = ringBuffer[tail];
+    tail = (tail + 1) % BUFFER_SIZE;
+
+    if (currentChar == '\n')
+    { // End of message
+      break;
+    }
+
+    message[index++] = currentChar;
+  }
+
+  message[index] = '\0'; // Null-terminate the message string
+  Serial.println(message);
+
+  // Parse the extracted message
+  parseMessage(message);
+}
+
 void setup()
 {
   pinMode(enaPin, OUTPUT);
@@ -88,24 +154,29 @@ void setup()
   digitalWrite(in2Pin, LOW);
   analogWrite(enaPin, 0);
   Serial.begin(9600);
-  
 }
 
 int a = 0;
 
 void loop()
 {
-  Serial.println(set_degree);
-  if (Serial.available() > 0)
+  while (Serial.available() > 0)
   {
-    a = Serial.read();
-    if (a == '1')
+    char incomingByte = Serial.read();
+    Serial.print(incomingByte);
+    ringBuffer[head] = incomingByte;
+    head = ++head % BUFFER_SIZE; // Move the head and wrap it around
+
+    // If head meets tail, it means buffer overflow, so move tail forward
+    if (head == tail)
     {
-      set_degree++;
+      tail = ++tail % BUFFER_SIZE;
     }
-    else if (a == '2')
+
+    // Check for the end of the message (newline '\n')
+    if (incomingByte == '\n')
     {
-      set_degree--;
+      processMessage();
     }
   }
   steer(set_degree);
