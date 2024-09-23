12eec55c08c4d130e1e10a36e8ebfdb580e143a8
Jesse Born <jesse.born@gmx.ch>
2024-09-23 21:05:39 +0200

    fix round dir

--- raspy/rounddir.py
+++ raspy/rounddir.py
@@ -9,7 +9,7 @@ def find_round_dir(black_img: np.ndarray):
   upper = 90
   edges_img = cv2.Canny(black_img, lower, upper, 3)
   # make the bottom row all white, so we don't detect the floor
-  edges_img = cv2.line(edges_img, (0, edges_img.shape[0]-1), (edges_img.shape[1], edges_img.shape[0]-1), 1, 1)
+  edges_img = cv2.line(edges_img, (0, 0), (edges_img.shape[1], 0), 1, 1)
 
   wall_heights = np.argmax(edges_img, axis=0)
 
@@ -26,6 +26,6 @@ def find_round_dir(black_img: np.ndarray):
   MIN_JUMP = 17
   counter_clockwise = np.sum(differences > MIN_JUMP)
   clockwise = np.sum(differences < -MIN_JUMP)
-  print("cc", counter_clockwise)
-  print("cw", clockwise)
-  return 1 if clockwise > counter_clockwise else -1
+  # print("cc", counter_clockwise)
+  # print("cw", clockwise)
+  return -1 if clockwise > counter_clockwise else 1
